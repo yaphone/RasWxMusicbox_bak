39ea5c5d4352ef0f8474b21449a3ca528cc9001e
yaphone <zhouyafeng2013@gmail.com>
2016-06-02 22:20:38 +0800

    s

--- netease.py
+++ netease.py
@@ -1,8 +1,6 @@
 #coding=utf-8
 from api import NetEase
-import mp3play
 import time
-import pyglet
 import webbrowser
 
 class RasWxMusicbox:
@@ -15,17 +13,17 @@ class RasWxMusicbox:
         if 'songs' in data['result']:
             if 'mp3Url' in data['result']['songs']:
                 songs = data['result']['songs']
-                
+
             else:
                 for i in range(0, len(data['result']['songs']) ):
                     song_ids.append( data['result']['songs'][i]['id'] )
                 songs = netease.songs_detail(song_ids)
-        self.mp3_data = netease.dig_info(songs, 'songs')   #歌曲信息，album, artist, song_name, mp3_url        
-    
+        self.mp3_data = netease.dig_info(songs, 'songs')   #歌曲信息，album, artist, song_name, mp3_url
+
     def get_music(self, index):
         return self.mp3_data[index]
-        
-        
+
+
     def gen_music_list(self):
         #最多显示10条歌曲信息
         music_list = ''
@@ -34,9 +32,9 @@ class RasWxMusicbox:
             music_list += '**' + str(i) + '**' + u'专辑：' + self.mp3_data[i]['album_name'] + '\n' \
                         + u'艺术家：' + self.mp3_data[i]['artist'] + '\n' \
                         + u'歌曲名：' + self.mp3_data[i]['song_name'] + '\n' \
-                        + '-------------------' + '\n' 
+                        + '-------------------' + '\n'
         return music_list
-    
+
     def get_music_url(self, song_id):
         if song_id > 9:
             pass
@@ -44,24 +42,24 @@ class RasWxMusicbox:
             return self.mp3_data[song_id]['mp3_url']
         except:
             pass
-        
-            
-        
+
+
+
 '''
-        
+
         for mp3_url  in mp3_url_data:
             print mp3_url['album_name']
             print mp3_url['artist']
             print mp3_url['song_name']
             print mp3_url['mp3_url']
             print '---------------------------'
-            
+
 '''
 
 if __name__ == '__main__':
     mb = RasWxMusicbox(u'南山南')
     print mb.get_music(2)
-    
-    
-    
+
+
+
 #    webbrowser.open(mp3_url)
--- run.py
+++ run.py
@@ -1,5 +1,5 @@
+#!/usr/bin/python
 #coding=utf-8
-
 import time
 import itchat
 import netease
@@ -9,7 +9,7 @@ import subprocess
 import webbrowser
 
 itchat.auto_login()
-will_play_list = []   #播放列表
+will_play_list = []
 process = None
 
 
@@ -23,7 +23,7 @@ def musicbox():
             if key_word == 'R':
                 all_music_str = ''
                 if len(will_play_list) == 0:
-                    all_music_str = u'播放列表空' 
+                    all_music_str = u'播放列表空'
                 for song in will_play_list:
                     all_music_str += song['song_name'] + '\n'
                 return all_music_str
@@ -37,19 +37,19 @@ def musicbox():
                 content_list = content.split()
                 song_name = content_list[1]
                 musicbox = netease.RasWxMusicbox(song_name)
-                
+
                 if len(content_list) == 2:
                     music_list = musicbox.gen_music_list()
                     return music_list
                 if len(content_list) == 3:
-                    music_index = int(content_list[2])   
+                    music_index = int(content_list[2])
                     music_info = musicbox.get_music(music_index)
                     will_play_list.append(music_info)
                     return u'添加' + music_info['song_name'] + u'成功'
         else:
             pass
     itchat.run()
-    
+
 def play():
     if len(will_play_list) == 0:
         return u'播放列表空'
@@ -58,13 +58,17 @@ def play():
         song_name = song['song_name']
         mp3_url = song['mp3_url']
         try:
-            os.kill(process.pid)
+            subprocess.Popen(['pkill', 'mpg123'])
+            time.sleep(.3)
         except:
             pass
-        process = subprocess.Popen(['mpg123', mp3_url])
+        finally:
+            process = subprocess.Popen(['mpg123', mp3_url])
+
+
 #        webbrowser.open(mp3_url)
         return u'正在播放 %s' % song_name
-    
+
 if __name__ == '__main__':
     musicbox()
-       
+
