d41be871ce4b9775af8dcc30042e52c13e5bb5da
yaphone <zhouyaphone@163.com>
2016-06-04 19:25:25 +0800

    s

--- tmp.py
+++ tmp.py
@@ -1,7 +1,2 @@
-#coding=utf-8
-
-import eyed3
-
-mp3 = 'http://m2.music.126.net/GkFXC6qt6rEU2q9KYU-6yQ==/1373290025677371.mp3'
-tmp = eyed3.load(mp3)
-print tmp.info.time_sec
+tmp = '1 2 3'
+print tmp.split()
